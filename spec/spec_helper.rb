c0464d47321f43da767a9ec62ec1da93569717e1
Leah Riffell <leah.riffell@gmail.com>
2020-10-29 20:07:21 -0600

    Clean up comments in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,6 @@
 ENV["SINATRA_ENV"] = "test"
 
 require 'simplecov'
-
-SimpleCov.start 'rails' do
-  add_filter '/config/'
-  add_filter '/spec/'
-end
-
 require 'shoulda-matchers'
 require 'capybara/dsl'
 require 'rack/test'
@@ -15,12 +9,10 @@ require 'rspec'
 require 'webmock/rspec'
 require 'vcr'
 
-#
-# if ActiveRecord::Migrator.needs_migration?
-#   raise 'Migrations are pending. Run `rake db:migrate SINATRA_ENV=test` to resolve the issue.'
-# end
-#
-# ActiveRecord::Base.logger = nil
+SimpleCov.start 'rails' do
+  add_filter '/config/'
+  add_filter '/spec/'
+end
 
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
@@ -29,16 +21,6 @@ RSpec.configure do |config|
 
   config.order = 'default'
 end
-#
-# def app
-#   Rack::Builder.parse_file('config.ru').first
-# end
-# Shoulda::Matchers.configure do |config|
-#   config.integrate do |with|
-#     with.test_framework :rspec
-#     # with.library :sinatra
-#   end
-# end
 
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
